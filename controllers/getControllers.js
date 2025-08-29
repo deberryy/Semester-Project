296bd4a435d416d41e29c881720bf45c0db55237
Patrick-123-FTY <pessuman020@st.ug.edu.gh>
2025-08-29 18:09:49 +0000

    updated goal model and goal controller

--- controllers/getControllers.js
+++ controllers/getControllers.js
@@ -1,54 +1,41 @@
 const asyncHandler = require('express-async-handler')
 
+
 // @desc   Get goals
 // @route  GET /api/goals
 // @access Private
-
-const getGoals = asyncHandler (async(req, res) => {
-    res.status(200).json({message: 'Get goals'})
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
 })
 
 // @desc   Set goal
-// @route  POST /api/goal
+// @route  POST /api/goals
 // @access Private
-
-const setGoals = asyncHandler(async(req, res) => {
-
-    if(!req.body.text) {
+const setGoals = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
-        
     }
-
-    res.status(200).json({message: 'Get goals'})
+    res.status(200).json({ message: 'Set goal' })
 })
 
-// @desc   Update goal goal
-// @route  update /api/goals/:id
+// @desc   Update goal
+// @route  PUT /api/goals/:id
 // @access Private
-
-const updateGoal = asyncHandler (async(req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}` })
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}` })
 })
 
 // @desc   Delete goal
-// @route  delete /api/goals/:id
+// @route  DELETE /api/goals/:id
 // @access Private
-
-const deleteGoal = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}` })
 })
 
-
-
-
-
-
-module.exports = { 
+module.exports = {
     getGoals,
     setGoals,
     updateGoal,
-    deleteGoal,
-    
-
+    deleteGoal
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 
-const port = process.env.PORT || 5000
+connectDB() // Connect to MongoDB
 
 const app = express()
 
@@ -10,4 +11,11 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+   
+
+    console.log(`Server started on port ${port}`)
+}
+)
