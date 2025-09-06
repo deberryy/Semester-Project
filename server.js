92a498c038da28eb68876a36dbde18bd5003435e
Patrick-123-FTY <pessuman020@st.ug.edu.gh>
2025-09-06 21:22:55 +0000

    newest update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
+const cors = require('cors') // Import cors middleware
 const userRoutes = require('./routes/userRoutes')
 const paymentRoutes = require('./routes/paymentRoutes') // Import new routes
 const app = express()
@@ -11,6 +12,9 @@ connectDB()
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// Enable CORS for all requests
+app.use(cors())
 
 // Routes
 app.use('/api/users', userRoutes)
